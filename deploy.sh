80a6182f3e6a6832f2d277bcd448e467adf4164b
Felix Kaaman <tmtu@tmtu.ee>
2024-06-07 19:12:23 +0200

    update deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,9 @@
 set -Eeuo pipefail
 set -v
 
-cargo build --target armv7-unknown-linux-musleabihf
-sshpass -p "$PASS" ssh -t root@odroid 'systemctl stop rut'
-sshpass -p "$PASS" scp target/armv7-unknown-linux-musleabihf/debug/rut root@odroid:/usr/local/bin
-sshpass -p "$PASS" scp index.html root@odroid:/home/rut
-sshpass -p "$PASS" ssh -t root@odroid 'systemctl start rut'
+# cargo build --target armv7-unknown-linux-musleabihf
+# sshpass -p "$PASS" ssh -t tmtu@rascalberry 'systemctl stop rut'
+# sshpass -p "$PASS" scp target/armv7-unknown-linux-musleabihf/debug/rut tmtu@rascalberry:/usr/local/bin
+sshpass -p "$PASS" scp index.html tmtu@rascalberry:/home/tmtu
+sshpass -p "$PASS" ssh -t tmtu@rascalberry 'sudo mv /home/tmtu/index.html /var/lib/rut/index.html'
+# sshpass -p "$PASS" ssh -t tmtu@rascalberry 'systemctl start rut'
